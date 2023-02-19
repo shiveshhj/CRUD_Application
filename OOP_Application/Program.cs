e782d09cf954fe9830ff6efbe5cb65a7fe0bd67b
HaidukEvgen <kanura56@gmail.com>
2023-02-19 22:19:31 +0300

    DPI problem fixed

--- OOP_Application/Program.cs
+++ OOP_Application/Program.cs
@@ -8,15 +8,18 @@ namespace OOP_Application
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
